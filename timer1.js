4906988bbdd1b9f83cbabc934a0c6b309c704a38
casper306 <perfect306@gmail.com>
2021-07-27 22:19:13 +0000

    add timer1.js

---
+++ timer1.js
@@ -0,0 +1,16 @@
+
+const args = process.argv.slice(2);
+const timer = function(){
+
+  for (const time of args){
+  if(time < 0 || isNaN(time)){
+    return 'err';
+  }
+
+  setTimeout(() => {
+    process.stdout.write('\x07');
+console.log(time);
+  }, time*1000)
+} return;
+};
+timer();
